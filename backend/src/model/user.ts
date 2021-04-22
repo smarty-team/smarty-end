d1f266a0d2891858d90c2bbbf65e6a6094ec0d0e
josephxia <15906475@qq.com>
2021-04-22 10:22:41 +0800

    update

--- backend/src/model/user.ts
+++ backend/src/model/user.ts
@@ -1,10 +1,14 @@
 export default {
-    name: '用户管理',
+    description: '用户管理',
     schema: {
-        mobile: { name: '手机号',type: String, unique: true, required: true },
-        password: { name: '密码',type: String, required: true  },
-        realName: { name: '真实姓名',type: String, required: true },
-        avatar: { name: '头像',type: String, default: 'https://1.gravatar.com/avatar/a3e54af3cb6e157e496ae430aed4f4a3?s=96&d=mm' },
-        createdAt: { name: '创建时间',type: Date, default: Date.now },
+        mobile: { description: '手机号', type: String, unique: true, required: true },
+        password: { description: '密码', type: String, required: true },
+        realName: { description: '真实姓名', type: String, required: true },
+        avatar: {
+            description: '头像',
+            type: String,
+            default: 'https://1.gravatar.com/avatar/a3e54af3cb6e157e496ae430aed4f4a3?s=96&d=mm',
+        },
+        createdAt: { description: '创建时间', type: Date, default: Date.now },
     },
 }
