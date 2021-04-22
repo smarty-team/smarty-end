bf1730648e5f84e1991693efb85d6848aac64ab3
josephxia <15906475@qq.com>
2021-04-22 17:15:55 +0800

    update

--- backend/src/model/user.ts
+++ backend/src/model/user.ts
@@ -1,14 +1,13 @@
 export default {
     description: '用户管理',
     schema: {
-        mobile: { description: '手机号', type: String, unique: true, required: true },
-        password: { description: '密码', type: String, required: true },
-        realName: { description: '真实姓名', type: String, required: true },
+        mobile: { description: '手机号', type: 'String', unique: true, required: true },
+        password: { description: '密码', type: 'String', required: true },
+        realName: { description: '真实姓名', type: 'String', required: true },
         avatar: {
             description: '头像',
-            type: String,
+            type: 'String',
             default: 'https://1.gravatar.com/avatar/a3e54af3cb6e157e496ae430aed4f4a3?s=96&d=mm',
         },
-        createdAt: { description: '创建时间', type: Date, default: Date.now },
     },
 }
